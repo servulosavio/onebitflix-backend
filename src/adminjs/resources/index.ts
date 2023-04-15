4575ce5310e51e46a6d650b8fbbf608c69696c5e
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-04-14 23:21:14 -0300

    Upload de arquivos no AdminJS

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,15 +1,22 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
     {
         resource: Category,
-        options: categoryResourceOptions
+        options: categoryResourceOptions,
     },
     {
         resource: Course,
         options: courseResourceOptions
-    }
+    },
+    {
+        resource: Episode,
+        options: episodeResourceOptions,
+        features: episodeResourceFeatures
+    },
+
 ]
