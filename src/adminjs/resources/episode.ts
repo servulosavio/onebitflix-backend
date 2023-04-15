4cbad4cf201bd3802eb6dff19ec9e29eab218caa
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-04-15 00:13:08 -0300

    Upload das capas dos cursos

--- src/adminjs/resources/episode.ts
+++ src/adminjs/resources/episode.ts
@@ -14,7 +14,7 @@ export const episodeResourceFeatures: FeatureType[] = [
     uploadFileFeature({
         provider: {
             local: {
-                bucket: path.join(__dirname, '..', '..', '..', 'uploads')
+                bucket: path.join(__dirname, '../../../uploads')
             }
         },
         properties: {
