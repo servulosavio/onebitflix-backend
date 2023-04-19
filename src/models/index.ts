9416d0dc5ab234e314df06bb08918a65c8903c84
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-04-19 19:53:01 -0300

    Obtendo 3 cursos em destaque

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,7 +6,7 @@ import { User } from "./User";
 Category.hasMany(Course, { as: 'courses' })
 Course.belongsTo(Category)
 
-Course.hasMany(Episode) //Episodes
+Course.hasMany(Episode, { as: 'episodes' })
 Episode.belongsTo(Course)
 
 export {
--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ const router = express.Router()
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
 
+router.get('/courses/featured', coursesController.featured)
 router.get(':courses/:id', coursesController.show)
 
 export { router }
