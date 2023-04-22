25f60c74596c68ff1e183c258c77b946907be51c
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-04-21 22:56:07 -0300

    Protegendo os vídeos

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import exp from 'constants';
 import { coursesController } from './controllers/coursesController';
 import { episodesController } from './controllers/episodesControllers';
 import { authController } from './controllers/authController';
-import { ensureAuth } from './middlewares/auth';
+import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth';
 
 const router = express.Router()
 
@@ -19,5 +19,5 @@ router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', ensureAuth, coursesController.search)
 router.get(':courses/:id', ensureAuth, coursesController.show)
 
-router.get('/episodes/stream', episodesController.stream)
+router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
 export { router }
